3711074eb506129e2c480710310f4790da553833
saifoelloh <saifoelloh@gmail.com>
2020-03-09 23:31:51 +0700

    creating kelas and peserta initial pages

--- src/pages/Kelas/index.js
+++ src/pages/Kelas/index.js
@@ -1,9 +1,60 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from 'react'
+import { Button, Col, Container, Row, Table } from 'reactstrap'
 
-export default function Kelas() {
+import ExampleWrapperSimple from '../../layout-components/ExampleWrapperSimple/index.js'
+
+const Kelas = () => {
+  const tHead = ['no', 'name', 'phone', 'class', 'status', 'actions']
+  const tBody = [
+    {
+      id: 1041,
+      name: 'saipul',
+      phone: '012470174',
+      class: 'web',
+      status: 'bayar'
+    }
+  ]
+
   return (
     <Fragment>
-      <h1>Hello World</h1>
+      <Row>
+        <Col>
+          <ExampleWrapperSimple sectionHeading="Kelas">
+            <Container>
+              <Table bordered hover>
+                <thead>
+                  <tr>
+                    {tHead.map((h, id) => (
+                      <td key={id}>{h}</td>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {tBody.map((datum, id) => (
+                    <tr key={id}>
+                      <td>{(id += 1)}</td>
+                      <td>{datum.name}</td>
+                      <td>{datum.phone}</td>
+                      <td>{datum.class}</td>
+                      <td>{datum.status}</td>
+                      <td>
+                        <Button
+                          size="sm"
+                          color="primary"
+                          to={() => `/payment/${datum.id}`}>
+                          Validasi
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Container>
+          </ExampleWrapperSimple>
+        </Col>
+      </Row>
     </Fragment>
   )
 }
+
+export default Kelas
---
+++ src/pages/Peserta/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import ExampleWrapperSimple from '../../layout-components/ExampleWrapperSimple/index.js'
+
+const Peserta = () => {
+  return (
+    <ExampleWrapperSimple sectionHeading="Peserta">
+      <h1>Hello</h1>
+    </ExampleWrapperSimple>
+  )
+}
+
+export default Peserta
