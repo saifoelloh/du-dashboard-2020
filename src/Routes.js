13e6815f56a7883e0c2a54e5dcd96e596c16f14f
saifoelloh <saifoelloh@gmail.com>
2020-03-10 22:58:52 +0700

    removing unnecessary things

--- src/Routes.js
+++ src/Routes.js
@@ -1,45 +1,42 @@
-import { AnimatePresence, motion } from 'framer-motion';
-import React, { Suspense, lazy } from 'react';
-import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion'
+import React, { Suspense, lazy } from 'react'
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom'
 
-import Badges from './example-pages/Badges';
-import Buttons from './example-pages/Buttons';
-import Dropdowns from './example-pages/Dropdowns';
-import FormsControls from './example-pages/FormsControls';
-import FormsLayout from './example-pages/FormsLayout';
-import Icons from './example-pages/Icons';
-import NavigationMenus from './example-pages/NavigationMenus';
-import Pagination from './example-pages/Pagination';
-import ProgressBars from './example-pages/ProgressBars';
-import RegularTables1 from './example-pages/RegularTables1';
-import RegularTables4 from './example-pages/RegularTables4';
-import Scrollable from './example-pages/Scrollable';
-import UtilitiesHelpers from './example-pages/UtilitiesHelpers';
-import Kelas from './pages/Kelas/index.js';
-import Peserta from './pages/Peserta/index.js';
-import { LeftSidebar, PresentationLayout } from './layout-blueprints';
+import Badges from './example-pages/Badges'
+import Buttons from './example-pages/Buttons'
+import Dropdowns from './example-pages/Dropdowns'
+import FormsControls from './example-pages/FormsControls'
+import FormsLayout from './example-pages/FormsLayout'
+import Icons from './example-pages/Icons'
+import NavigationMenus from './example-pages/NavigationMenus'
+import Pagination from './example-pages/Pagination'
+import ProgressBars from './example-pages/ProgressBars'
+import RegularTables1 from './example-pages/RegularTables1'
+import RegularTables4 from './example-pages/RegularTables4'
+import Scrollable from './example-pages/Scrollable'
+import UtilitiesHelpers from './example-pages/UtilitiesHelpers'
+import Kelas from './pages/Kelas/index.js'
+import Peserta from './pages/Peserta/index.js'
+import { LeftSidebar } from './layout-blueprints'
 
 // Layout Blueprints
 
-
 // Example Pages
 
-
-const DashboardDefault = lazy(() => import('./example-pages/DashboardDefault'));
-const Cards3 = lazy(() => import('./example-pages/Cards3'));
-const ListGroups = lazy(() => import('./example-pages/ListGroups'));
-const LandingPage = lazy(() => import('./example-pages/LandingPage'));
-const Modals = lazy(() => import('./example-pages/Modals'));
-const Notifications = lazy(() => import('./example-pages/Notifications'));
-const Carousels = lazy(() => import('./example-pages/Carousels'));
-const Popovers = lazy(() => import('./example-pages/Popovers'));
-const Tooltips = lazy(() => import('./example-pages/Tooltips'));
-const Tabs = lazy(() => import('./example-pages/Tabs'));
-const ApexCharts = lazy(() => import('./example-pages/ApexCharts'));
-const Maps = lazy(() => import('./example-pages/Maps'));
+const DashboardDefault = lazy(() => import('./example-pages/DashboardDefault'))
+const Cards3 = lazy(() => import('./example-pages/Cards3'))
+const ListGroups = lazy(() => import('./example-pages/ListGroups'))
+const Modals = lazy(() => import('./example-pages/Modals'))
+const Notifications = lazy(() => import('./example-pages/Notifications'))
+const Carousels = lazy(() => import('./example-pages/Carousels'))
+const Popovers = lazy(() => import('./example-pages/Popovers'))
+const Tooltips = lazy(() => import('./example-pages/Tooltips'))
+const Tabs = lazy(() => import('./example-pages/Tabs'))
+const ApexCharts = lazy(() => import('./example-pages/ApexCharts'))
+const Maps = lazy(() => import('./example-pages/Maps'))
 
 const Routes = () => {
-  const location = useLocation();
+  const location = useLocation()
 
   const pageVariants = {
     initial: {
@@ -54,13 +51,13 @@ const Routes = () => {
       opacity: 0,
       scale: 1.01
     }
-  };
+  }
 
   const pageTransition = {
     type: 'tween',
     ease: 'anticipate',
     duration: 0.4
-  };
+  }
 
   return (
     <AnimatePresence>
@@ -74,21 +71,6 @@ const Routes = () => {
         }>
         <Switch>
           <Redirect exact from="/" to="/dashboard" />
-          <Route path={['/LandingPage']}>
-            <PresentationLayout>
-              <Switch location={location} key={location.pathname}>
-                <motion.div
-                  initial="initial"
-                  animate="in"
-                  exit="out"
-                  variants={pageVariants}
-                  transition={pageTransition}>
-                  <Route path="/LandingPage" component={LandingPage} />
-                </motion.div>
-              </Switch>
-            </PresentationLayout>
-          </Route>
-
           <Route
             path={[
               '/dashboard',
@@ -162,7 +144,7 @@ const Routes = () => {
         </Switch>
       </Suspense>
     </AnimatePresence>
-  );
-};
+  )
+}
 
-export default Routes;
+export default Routes
